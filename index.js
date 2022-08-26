671b7b748a02632d47c47b70ecd68cfdea8a23d6
rebecca-2022 <rebecca.njeri@student.moringaschool.com>
2022-08-26 20:26:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const { returns } = require("chai-spies");
+// Add your code here
+function submitData() {
+    
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: 'Steve',
+            email: 'steve@steve.com'
+        })
+    } )
+    .then( function ( users ) {
+        return users.json()
+    } )
+    .then( function ( object ) {
+        document.body.textContent = object["id"]
+    } )
+    .catch( function ( error ) {
+        document.body.textContent = error.message
+    } )
+}
+submitData(name, email);
+Footer
